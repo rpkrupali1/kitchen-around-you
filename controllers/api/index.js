44fb79908eedd386e6dd77f87335f0985d1e8699
krupali pilgulwar <rpkrupali@gmail.com>
2022-03-12 07:57:59 -0600

    created all routes for ingredients

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,8 +3,8 @@ const ingredientRoutes = require("./ingredient-routes");
 const userRoutes = require("./user-routes");
 const measurementRoutes = require("./measurment-routes");
 
-//router.use('/ingredients', ingredientRoutes);
 router.use("/users", userRoutes);
 router.use("/measurments", measurementRoutes);
+router.use("/ingredients", ingredientRoutes);
 
 module.exports = router;
