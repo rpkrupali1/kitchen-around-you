d62ac6bd085ecc1e96110c10a1b59694e165010e
themadtaco <aatrox48@gmail.com>
2022-03-14 18:14:58 -0500

    added program template

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { User, Program, Registration } = require('../models');
+const { User, Program, Registration, ProgramDetail, Ingredient, Measurment } = require('../models');
 
 // Get homepage handlebar template
 router.get('/', (req, res) => {
@@ -13,6 +13,8 @@ router.get('/', (req, res) => {
         ]
     })
     .then(dbProgramData => {
+        // take programs and serialize them into a seperate programs array to be used
+        // in the template
         const programs = dbProgramData.map(program => program.get({ plain: true }));
 
         res.render('homepage', {
@@ -28,6 +30,55 @@ router.get('/', (req, res) => {
 // Get login handlebar template
 router.get('/login', (req, res) => {
     res.render('login');
+});
+
+// Get single program
+router.get('/program/:id', (req, res) => {
+    Program.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'description',
+            'tution',
+            'from_date',
+            'to_date'
+        ],
+        include: [
+            {
+                model: ProgramDetail,
+                attributes: ["program_id", "ingredient_id", "quantity"],
+                include: {
+                  model: Ingredient,
+                  attributes: ["name"],
+                  include: {
+                    model: Measurment,
+                    attributes: ["unit"],
+                  },
+                },
+              },
+        ]
+    })
+    .then(dbProgramData => {
+        if (!dbProgramData) {
+            res.status(404).json({ message: 'No program was found with this id'});
+            return;
+        }
+
+        // serialize data to return what is necessary
+        const program = dbProgramData.get({ plain: true });
+
+        // pass data to template
+        res.render('program', {
+            program
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
