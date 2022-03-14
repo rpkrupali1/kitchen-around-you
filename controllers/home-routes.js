9a9e74c515490d03efaa181f09e4eedb2abc3548
themadtaco <aatrox48@gmail.com>
2022-03-14 16:29:06 -0500

    finished login page template and js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,28 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User, Program, Registration } = require('../models');
 
 // Get homepage handlebar template
 router.get('/', (req, res) => {
-    
+    Program.findAll({
+        attributes: [
+            'title',
+            'tution',
+            'from_date',
+            'to_date'
+        ]
+    })
+    .then(dbProgramData => {
+        const programs = dbProgramData.map(program => program.get({ plain: true }));
+
+        res.render('homepage', {
+            programs
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 // Get login handlebar template
