83fe236a1819a70150050399d91b5e140e03ce02
krupali pilgulwar <rpkrupali@gmail.com>
2022-03-15 17:49:31 -0500

    fixed dashboard routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,58 +1,65 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Program, User, Registration, ProgramDetail, Ingredient, Measurment } = require('../models');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const {
+  Program,
+  User,
+  Registration,
+  ProgramDetail,
+  Ingredient,
+  Measurment,
+} = require("../models");
 
-router.get('/:id', (req, res) => {
-    User.findOne({
-        attributes: { exclude: ["password"] },
-        where: {
-          id: req.params.id,
-        },
-        include: [
-          {
-            model: Registration,
-            attributes: ["id"],
-            include: {
-              model: Program,
-              attributes: [
-                "id",
-                "title",
-                "description",
-                "tution",
-                "from_date",
-                "to_date",
-              ],
-              include: [
-                {
-                  model: ProgramDetail,
-                  attributes: ["program_id", "ingredient_id", "quantity"],
-                  include: {
-                    model: Ingredient,
-                    attributes: ["name"],
-                    include: {
-                      model: Measurment,
-                      attributes: ["unit"],
-                    },
-                  },
+router.get("/", (req, res) => {
+  User.findOne({
+    attributes: { exclude: ["password"] },
+    where: {
+      id: req.session.user_id,
+    },
+    include: [
+      {
+        model: Registration,
+        attributes: ["id"],
+        include: {
+          model: Program,
+          attributes: [
+            "id",
+            "title",
+            "description",
+            "tution",
+            "from_date",
+            "to_date",
+          ],
+          include: [
+            {
+              model: ProgramDetail,
+              attributes: ["program_id", "ingredient_id", "quantity"],
+              include: {
+                model: Ingredient,
+                attributes: ["name"],
+                include: {
+                  model: Measurment,
+                  attributes: ["unit"],
                 },
-              ],
+              },
             },
-          },
-        ],
-      })
-        .then((dbUserData) => {
-          if (!dbUserData) {
-            res.status(404).json({ message: "No user found with this id" });
-            return;
-          }
-          const user = dbUserData.get({ plain: true })
-          console.log(user);
-          res.render('dashboard', user);
-        })
-        .catch((err) => {
-          console.log(err);
-          res.status(500).json(err);
-        });
+          ],
+        },
+      },
+    ],
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      const user = dbUserData.get({ plain: true });
+      console.log(user);
+      res.render("dashboard", user);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
